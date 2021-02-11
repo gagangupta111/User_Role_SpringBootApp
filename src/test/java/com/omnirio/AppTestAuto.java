b61c2a1f7c737de3ae45692144dd4c1f20ab5a12
GaganGupta <gagan_gupta111@yahoo.com>
2021-02-11 12:32:24 +0530

    Test methods

--- src/test/java/com/omnirio/AppTestAuto.java
+++ src/test/java/com/omnirio/AppTestAuto.java
@@ -1,7 +1,9 @@
 package com.omnirio;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -20,10 +22,42 @@ public class AppTestAuto {
 	@Autowired
 	private MockMvc mvc;
 
+	// Create User
 	@Test
-	public void getHello() throws Exception {
+	public void createUser1() throws Exception {
+
+		mvc.perform(
+			MockMvcRequestBuilders.post("/omnirio/user")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content("{\n" +
+					"    \"branch\":\"A\",\n" +
+					"    \"dob\":\"12/12/1988\"\n" +
+					"}")
+			.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.User.dob").value("12/12/1988"));
+
+	}
+
+	@Test
+	public void createUser2() throws Exception {
+
+		mvc.perform(
+				MockMvcRequestBuilders.post("/omnirio/user")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content("{}")
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.User", hasKey("userID")));
+	}
+
+	// get all users
+	@Test
+	public void getUsers() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/omnirio/user").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("ABC")));
+				.andExpect(jsonPath("$.User").isArray())
+				.andExpect(jsonPath("$.User", hasSize(2)));
 	}
+
 }
